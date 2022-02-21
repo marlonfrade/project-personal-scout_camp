4c3c17b248a95429b930ef9c25abe5babdcb3abd
Marlon Frade <marlonmelo98@hotmail.com>
2022-02-20 23:04:34 -0300

    working on the middleware of reviews delete

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const { campgroundSchema } = require("../schemas");
+const Review = require("./review");
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -15,6 +17,26 @@ const CampgroundSchema = new Schema({
       ref: "Review",
     },
   ],
+});
+
+// Criaremos uma função teste para validar que ao usuário remover um acampamento possamos também remover as avaliações inseridas naquele acampamento em específico
+CampgroundSchema.post("findOneAndDelete", async function (doc) {
+  // Daremos um console.log para verificar se ao clicarmos em remover um acampamento,
+  // Temos como retorno dentro do terminal o nosso console, afirmando que esta tudo certo e podemos seguir com a lógica para remover de fato
+  // console.log("DELETE");
+
+  // depois de verificar, adicionamos na função assíncrona uma variável doc, e podemos dar um console nele para ver exatamente o que foi removido,
+  // Então podemos utilizar da lógica de capturar o id do acampamento e remover a partir dele as avaliações criadas.
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
+  // depois de adicionar a lógica, para testar, vamos ate um acampamento e adicionamos algumas avaliações,
+  // Depois antes de remover vamos ate o mongo e filtramos por db.reviews.find(), e verificamos se os reviews foram adicionados,
+  // Depois removemos os acampamentos, filtramos no banco mongo novamente, e o que esperamos é que todos os reviews tenham sido removidos.
 });
 
 //Exportando o módulo para um modelo:
