05d258745b44fed1a8ab13123f4a041ae690eadc
Marlon Frade <marlonmelo98@hotmail.com>
2022-03-09 16:18:15 -0300

    Deploy production

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // npm i dotenv
 // env config
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+// if (process.env.NODE_ENV !== "production") {
+//   require("dotenv").config();
+// }
 console.log(process.env.CLOUDINARY_CLOUD_NAME);
 console.log(process.env.CLOUDINARY_KEY);
 console.log(process.env.CLOUDINARY_SECRET);
