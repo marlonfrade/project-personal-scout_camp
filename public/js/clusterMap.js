4e1f532a1a7856c5911136cf2080aba7fbb82675
Marlon Frade <marlonmelo98@hotmail.com>
2022-03-10 15:36:54 -0300

    After Deploy Adjusts

--- public/js/clusterMap.js
+++ public/js/clusterMap.js
@@ -1,9 +1,9 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "cluster-map",
-  style: "mapbox://styles/mapbox/light-v10",
+  style: "mapbox://styles/mapbox/dark-v10",
   center: [-103.5917, 40.6699],
-  zoom: 3,
+  zoom: 1,
 });
 
 // Add controls to the map
